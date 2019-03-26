8545bfad88158b3bd47db1536949d0d137d8b455
donaldsoundy <donald.soundy@ttu.edu>
2019-03-26 04:16:16 -0500

    Add files via upload

---
+++ Threadtesting.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define NUM_PRODUCERS 2
+#define NUM_CONSUMERS 2
+#define BUFFER_SIZE 12
+int inv = 0;
+pthread_mutex_t mute
+pthread_cond_t conc, conp;
+
+void *producer(void *ptr){
+	int item;
+	
+	while(i <= BUFFER_SIZE){
+		pthread_mutex_lock(&mute);
+		while(inv != 0) pthread_cond_wait(&conp, &mute);
+		inv = item;
+		pthread_cond_signal(&conc);
+		pthread_mutex_unlock(&mute);
+		
+		i++;
+	}
+	pthread_exit(0);
+}
+
+void *comsumer(void *ptr){
+	int item;
+	
+	while(item <=  BUFFER_SIZE){
+		pthread_mutex_lock(&mute);
+		while(inv==0) pthread_cond_wait(&conc, &mute);
+		buffer = 0;
+		pthread_cond_signal(&conp);
+		pthread_mutex_unlock(&mute);
+		
+		i++
+	}
+	pthread_exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	pthread_t pro, con;
+	pthread_mutex_init(&mute, 0);
+	pthread_cond_init(&conc, 0);
+	pthread_cond_init(&conp, 0);
+	pthread_create(&con, 0, consumer, 0);
+	pthread_create(&pro, 0, producer, 0);
+	pthread_join(pro, 0);
+	pthread_join(con, 0);
+	pthread_cond_destroy(&conc);
+	pthread_cond_destroy(&conp);
+	pthread_mutex_destroy(&the_mutex);
+	
+	return 0;
+}
