3f0353d2d35693500a4d020f75a31afa4e6a1c73
donaldsoundy <donald.soundy@ttu.edu>
2019-03-26 06:39:50 -0500

    Add files via upload

---
+++ greeting.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+temph=`date | cut -c19-20`
+dat=`date +"%A %d in %B of %Y (%r)"`
+
+if [ $temph -lt 12 ]
+then
+    msg="Good Morning $LOGNAME, Have nice day!"
+fi
+
+if [ $temph -ge 12 -a $temph -le 16 ]
+then
+    msg="Good Afternoon $LOGNAME"
+fi
+
+if [ $temph -gt 16 -a $temph -le 20 ]
+then
+    msg="Good Evening $LOGNAME"
+fi
+
+if [ $temph -gt 20 ]
+then
+	msg="Hello $LOGNAME"
+fi
+
+#echo $temph
+#echo $dat
+echo -e "$msg\nThis is $dat"
