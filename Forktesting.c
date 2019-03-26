a0ebcea7586df12f0ac0630375d522cc163f8ec4
donaldsoundy <donald.soundy@ttu.edu>
2019-03-26 07:40:53 -0500

    Update Forktesting.c

--- Forktesting.c
+++ Forktesting.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#define NUM_CHILDREN 8
+#define NUM_CHILDREN 8								//Sets the number of children processes you want to have
 
 int main(int argc, char *argv[])
 {
-	pid_t parentPid, childPid;
-	int s= 0;
+	pid_t parentPid, childPid;						//Initializing pids for later use
+	int s= 0;								//A status flag for the wait function
 	
-	printf("I am parent: %d\n", (int) getpid());
-	int parent = (int) getpid();
+	printf("I am parent: %d\n", (int) getpid());				//gives the parent's id number
+	int parent = (int) getpid();						//saves the parent's id number for later use
 	
-	int i = 0;
+	int i = 0;								//didn't want to do a for loop
 	
-	while(i<NUM_CHILDREN){
-		if ((childPid = fork()) < 0) {
-			printf("ERROR creating child. returned<0");
+	while(i<NUM_CHILDREN){							//loop to create children that say hello and their id number
+		if ((childPid = fork()) < 0) {					//for when a process doesnt work
+			printf("ERROR creating child. returned<0");		//shoots an error message if the child process doesnt run
 			exit(0);
 		}
-		if ((int) getpid() != parent) {
-			printf("Hello, PID: %d\n", (int) getpid());
-			exit(0);
+		if ((int) getpid() != parent) {					//we use the parent id to differentiate between the parent and children
+			printf("Hello, PID: %d\n", (int) getpid());		//children say hello and their id no
+			exit(0);						//children exit so as not to continue the while loop
 		}
-		i++;
+		i++;								//parent continues the while loop until all children are made
 	}
-	while((parentPid = wait(&s)) > 0);
+	while((parentPid = wait(&s)) > 0);					//parent waits for the children processes to end
 	return 0;
 }
