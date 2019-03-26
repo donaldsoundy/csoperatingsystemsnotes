a6d421f40de28f3f622368209a36513de6495754
donaldsoundy <donald.soundy@ttu.edu>
2019-03-25 21:59:08 -0500

    Add files via upload

--- Forktesting.c
+++ Forktesting.c
@@ -2,21 +2,29 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
-#define NUM_CHILDREN 2
+#define NUM_CHILDREN 8
 
 int main(int argc, char *argv[])
 {
+	pid_t parentPid, childPid;
+	int s= 0;
+	
 	printf("I am parent: %d\n", (int) getpid());
 	int parent = (int) getpid();
 	
-	while(i<NUM_CHILDREN){
-		if((int) getpid() == parent){
-		fork();
-		}
-	}
+	int i = 0;
 	
-	if((int) getpid() != parent){
-		printf("Hello, PID: %d\n", (int) getpid());
+	while(i<NUM_CHILDREN){
+		if ((childPid = fork()) < 0) {
+			printf("ERROR creating child. returned<0");
+			exit(0);
+		}
+		if ((int) getpid() != parent) {
+			printf("Hello, PID: %d\n", (int) getpid());
+			exit(0);
+		}
+		i++
 	}
+	while((parentPid = wait(&s)) > 0);
 	return 0;
 }
